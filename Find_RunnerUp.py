502994460802d1ff6b92a1404fd8a0e314f678ab
Akshata-IITR <akshatagaikwad13@gmail.com>
2026-08-23 20:18:05 +0530

    Change runner-up score calculation method
    Refactor to find runner-up score using sorted array and loop.

--- Find_RunnerUp.py
+++ Find_RunnerUp.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    unqarr= set(arr)
-    sortedscores=sorted(unqarr)
-    runner_up=sortedscores[-2]
-    print(runner_up)
+    array=sorted(arr, reverse=True)
+    i=0
+    while array[i]==array[i+1]:
+        i+=1
+    print(array[i+1])
