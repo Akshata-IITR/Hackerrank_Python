4537f853469db066c8d67c266d267c7418397ace
Akshata-IITR <akshatagaikwad13@gmail.com>
2026-08-23 19:38:24 +0530

    Refactor number printing to use string concatenation

--- Python_27_June.py
+++ Python_27_June.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-    m=[]
-    for i in range(1, n+1):
-        print(i,end="")
+    a=""
+    for i in range(1,n+1):
+        a+=str(i)
+    print(a)
