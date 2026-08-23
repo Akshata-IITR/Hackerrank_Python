e694d8569ce0d978bc4517d9872ff512db37eb9c
Akshata-IITR <akshatagaikwad13@gmail.com>
2026-08-23 19:34:42 +0530

    Refactor leap year calculation logic
    Refactor is_leap function for clarity and correctness.

--- Python_26_June.py
+++ Python_26_June.py
@@ -1,13 +1,16 @@
 def is_leap(year):
     leap = False
-    if year%400==0:
-        return True
-    elif year%100==0:
-        return False
-    elif year%4==0:
-        return True
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                return True
+            else:
+                return False
+        else:
+            return True
     else:
         return False
     return leap
 
 year = int(input())
+print(is_leap(year))
